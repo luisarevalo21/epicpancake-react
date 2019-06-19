1057b9594eac2dd25ded98369697b50ca0648fd6
Luis Arevalo <arevaloluis89@yahoo.com>
2019-06-18 21:25:39 -0700

    build filed added

---
+++ client/build/precache-manifest.3e8a33401de0fc38a2b08f9d15a4171f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbacee9f124d59efc750fe085e53b094",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ed15efa32f6839bfc2",
+    "url": "/static/css/2.7e8376dd.chunk.css"
+  },
+  {
+    "revision": "da2c52cffc76f73f08a0",
+    "url": "/static/css/main.868cd61a.chunk.css"
+  },
+  {
+    "revision": "95ed15efa32f6839bfc2",
+    "url": "/static/js/2.6a8e59c3.chunk.js"
+  },
+  {
+    "revision": "da2c52cffc76f73f08a0",
+    "url": "/static/js/main.61ef037e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "29e50bae5f405f9166ed9015f9a574e5",
+    "url": "/static/media/ep-logo-green.29e50bae.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 // Put all API endpoints under '/api'
-// app.get('/api/passwords', (req, res) => {
+app.get("/", (req, res) => {
+  console.log("hello world");
+});
 //   const count = 5;
 
 //   // Generate some passwords
@@ -23,8 +25,13 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+app.get("/", (req, res) => {
+  console.log("hello world");
+
+  // res.sendFile(path.join(__dirname + "/client/index.html"));
 });
 
 const port = process.env.PORT || 5000;
