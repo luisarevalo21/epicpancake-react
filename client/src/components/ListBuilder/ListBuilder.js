2341537d011ec98ed7f433297447349bf3fb2b3c
Luis Arevalo <arevaloluis89@yahoo.com>
2019-06-24 11:55:41 -0700

    added a loading screen

--- client/src/components/ListBuilder/ListBuilder.js
+++ client/src/components/ListBuilder/ListBuilder.js
@@ -4,49 +4,60 @@ import classes from "./ListBuilder.module.css";
 const listBuilder = props => {
   console.log(props.list);
   let list = null;
-  list = props.list.map(element => {
-    if (
-      element[6].trim() ===
-      "My friend will play for me (enter their name in the comment)"
-    ) {
-      return (
-        <li>
-          {element[0].trim() +
-            ", " +
-            element[1].trim() +
-            ", " +
-            element[3].trim() +
-            ", " +
-            "via proxy"}
-        </li>
-      );
-    } else if (
-      element[6].trim() ===
-      "I need someone to play for me (add your telegram name in the comments so an admin can contact you)"
-    ) {
-      return (
-        <li>
-          {element[0].trim() +
-            ", " +
-            element[1].trim() +
-            ", " +
-            element[3].trim() +
-            ", " +
-            "needs proxy"}
-        </li>
-      );
-    } else {
-      return (
-        <li>
-          {element[0].trim() +
-            ", " +
-            element[1].trim() +
-            ", " +
-            element[3].trim()}
-        </li>
-      );
-    }
-  });
+
+  if (props.list.length !== 0) {
+    list = props.list.map(element => {
+      if (
+        element[6].trim() ===
+        "My friend will play for me (enter their name in the comment)"
+      ) {
+        return (
+          <li>
+            {element[0].trim() +
+              ", " +
+              element[1].trim() +
+              ", " +
+              element[3].trim() +
+              ", " +
+              "via proxy"}
+          </li>
+        );
+      } else if (
+        element[6].trim() ===
+        "I need someone to play for me (add your telegram name in the comments so an admin can contact you)"
+      ) {
+        return (
+          <li>
+            {element[0].trim() +
+              ", " +
+              element[1].trim() +
+              ", " +
+              element[3].trim() +
+              ", " +
+              "needs proxy"}
+          </li>
+        );
+      } else {
+        return (
+          <li>
+            {element[0].trim() +
+              ", " +
+              element[1].trim() +
+              ", " +
+              element[3].trim()}
+          </li>
+        );
+      }
+    });
+  } else {
+    return (
+      <div className={[classes.List, "container"].join(" ")}>
+        <ul>
+          <li>No {props.team}</li>
+        </ul>
+      </div>
+    );
+  }
 
   return (
     <div className={[classes.List, "container"].join(" ")}>
--- client/src/containers/Gym/Gym.js
+++ client/src/containers/Gym/Gym.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Tabletop from "tabletop";
 import classes from "./Gym.module.css";
 import ListBuilder from "../../components/ListBuilder/ListBuilder";
+import Spinner from "../../components/Spinner/Spinner";
+
 class Gym extends Component {
   state = {
     // id: this.props.match.params.gymId,
@@ -10,7 +12,8 @@ class Gym extends Component {
     time: "",
     instinctList: [],
     valorList: [],
-    mysticList: []
+    mysticList: [],
+    loading: true
   };
 
   componentDidMount() {
@@ -24,26 +27,52 @@ class Gym extends Component {
     });
   }
   showInfo = (data, tabletop) => {
-    console.log("the id is", this.props.match.params.gymId);
     const array = tabletop.sheets(this.props.match.params.gymId).toArray();
-    const date = array[0][4];
-    const time = array[0][5];
-    const instinctList = array.filter(element => element[2] === "Instinct");
-    const valorList = array.filter(element => element[2] === "Valor");
-    const mysticList = array.filter(element => element[2] === "Mystic");
+    console.log("the array is", array);
+    if (array.length !== 0) {
+      console.log("the id is", this.props.match.params.gymId);
+      // const array = tabletop.sheets(this.props.match.params.gymId).toArray();
+      const date = array[0][4];
+      const time = array[0][5];
+      const instinctList = array.filter(element => element[2] === "Instinct");
+      const valorList = array.filter(element => element[2] === "Valor");
+      const mysticList = array.filter(element => element[2] === "Mystic");
 
-    console.log(array[0][0]);
-    this.setState({
-      data: array,
-      date,
-      time,
-      instinctList,
-      valorList,
-      mysticList
-    });
+      console.log(array[0][0]);
+      this.setState({
+        data: array,
+        date,
+        time,
+        instinctList,
+        valorList,
+        mysticList,
+        loading: false
+      });
+    }
   };
 
   render() {
+    let gyms = (
+      <>
+        {/* <h1 className={classes.Gym}>{this.props.match.params.gymId}</h1> */}
+        <div className={classes.List}>
+          <span className={classes.Gym_date_time}>{this.state.date}</span>
+          <span className={classes.Gym_date_time}>{this.state.time}</span>
+          <div className={[classes.List, classes.InstinctList].join(" ")}>
+            <ListBuilder list={this.state.instinctList} team="Instinct" />
+          </div>
+          <div className={[classes.List, classes.MysticList].join(" ")}>
+            <ListBuilder list={this.state.mysticList} team="Mystic" />
+          </div>
+          <div className={[classes.List, classes.ValorList].join(" ")}>
+            <ListBuilder list={this.state.valorList} team="Valor" />
+          </div>
+        </div>{" "}
+      </>
+    );
+    if (this.state.loading) {
+      gyms = <Spinner />;
+    }
     // console.log("the id is", this.state.data[0]);
     // let date = this.state.data[0];
     // this.state.data.map(element => {
@@ -53,19 +82,7 @@ class Gym extends Component {
     return (
       <div>
         <h1 className={classes.Gym}>{this.props.match.params.gymId}</h1>
-        <div className={classes.List}>
-          <span className={classes.Gym_date_time}>{this.state.date}</span>
-          <span className={classes.Gym_date_time}>{this.state.time}</span>
-          <div className={[classes.List, classes.InstinctList].join(" ")}>
-            <ListBuilder list={this.state.instinctList} />
-          </div>
-          <div className={[classes.List, classes.MysticList].join(" ")}>
-            <ListBuilder list={this.state.mysticList} />
-          </div>
-          <div className={[classes.List, classes.ValorList].join(" ")}>
-            <ListBuilder list={this.state.valorList} />
-          </div>
-        </div>
+        {gyms}
       </div>
     );
   }
